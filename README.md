1340f3823c921cdb6e65df0c292f858eee55109b
Serhii <sergeybutr@gmail.com>
2024-07-20 18:32:35 +0300

    New updates

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # OpenGL
+
+This is a project with basic payground for OpenGL APIs.
+
+# Project dependencies
+
+1) GLFW
+
+This is a library which implements some common platform dependent tasks for you like window creation.
+
+Ref: https://www.glfw.org/
+
+2) Glew
+
+This is a library which provides an access to platform dependent OpenGL APIs and features.
+It is responsible for locating & loading correct OpenGL library or functions.
+
+Ref: https://glew.sourceforge.net/
+
+# Platforms
+
+This is a Visual Studio project for windows platform
+
+# Some notes
+
+1) OpenGL Initialization
+
+This is a platform dependent process. It has 2 steps:
+
+	1. The creation of an OpenGL Context
+	2. Loading all the necessary functions to use OpenGL.
+
+	The 2 step is handled by Glew library.
+
+# Get started
+
+https://www.khronos.org/opengl/wiki/Getting_Started#Tutorials_and_How_To_Guides
+
+# OpenGL docs
+
+https://docs.gl/
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,20 +1,26 @@
+/* Glew is included first because it loads and provides OpenGL APIs to use later */
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 
+void printGLInfo();
+
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
+
     if (!glfwInit())
         return -1;
 
-    glewInit();
+    GLFWwindow* window;
+    int width = 1200;
+    int height = 800;
+    const char* title = "App";
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    window = glfwCreateWindow(width, height, title, NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -22,9 +28,11 @@ int main(void)
     }
 
     /* Make the window's context current */
+
     glfwMakeContextCurrent(window);
 
     /* Initializing GLEW */
+
     GLenum err = glewInit();
     if (GLEW_OK != err)
     {
@@ -33,22 +41,78 @@ int main(void)
         return -1;
     }
 
-    /* Print OpenGL version */
-    std::cout << glGetString(GL_VERSION) << std::endl;
+    /* Print OpenGL info */ 
+
+    printGLInfo();
+
+    /*
+        So what we need here is next.
+        We want to tell GL what to draw and how to draw.
+        For that we need to specify a data and give it to GL.
+
+    */
+
+    /* Create a vertex buffer. A vertex in this context just means a position or a point of a shape. */
+
+    const int size = 6;
+    float positions[size] = {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.0f, 0.5f
+    };
+    
+    unsigned int buffer;
+    glGenBuffers(1 /* Create 1 buffer */, &buffer /* buffer name or buffer id */);
+
+    /* Select vertex buffer */
+
+    glBindBuffer(GL_ARRAY_BUFFER /* Buffer type */, buffer /* Buffer name */);
+    
+    /* Fill a vertex buffer with data */
+    
+    glBufferData(
+        GL_ARRAY_BUFFER /* Buffer type */,
+        size * sizeof(float) /* Buffer size */,
+        positions,
+        GL_STATIC_DRAW /* A hint for GPU see docs for details */
+    );
 
     /* Loop until the user closes the window */
+
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Send a draw command */
+
+        glDrawArrays(GL_TRIANGLES, 0 /* Staring from the first point or vertex */, 3 /* Says that we have 3 points or 3 vetesies */);
+
         /* Swap front and back buffers */
+
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
+
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void printGLInfo() 
+{
+    auto version = glGetString(GL_VERSION);
+    if (version != nullptr)
+        std::cout << "GL version: " << version << std::endl;
+
+    auto vendor = glGetString(GL_VENDOR);
+    if (vendor != nullptr)
+        std::cout << "GL vendor: " << vendor << std::endl;
+
+    auto render = glGetString(GL_RENDER);
+    if (render != nullptr)
+        std::cout << "GPU name: " << render << std::endl;
 }
