631bab16db212a5b0ea251a24da9f74ccfe05a38
Serhii <sergeybutr@gmail.com>
2024-12-13 13:06:23 +0200

    Attempt to fix "Failed to init GLFW library"

--- engine/opengl/external/GLFBridge.cpp
+++ engine/opengl/external/GLFBridge.cpp
@@ -18,6 +18,10 @@ namespace xengine {
     {
         glfwSetErrorCallback(GLFWErrorCallback);
 
+        glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_NULL);
+        // glfwInitHint(GLFW_WAYLAND_DISABLE_LIBDECOR, GLFW_PLATFORM_NULL);
+        glfwInitHint(GLFW_WAYLAND_LIBDECOR /*GLFW_WAYLAND_LIBDECOR*/, GLFW_WAYLAND_DISABLE_LIBDECOR /*GLFW_WAYLAND_DISABLE_LIBDECOR*/);
+
         /* Initializes the GLFW library */
 
         if (!glfwInit()) {
